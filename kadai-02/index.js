843ed3bba404ee25dd11564bcd9bfdf37ac2c1d7
shimomura99 <shimomura99@users.noreply.github.com>
2021-10-25 07:45:18 +0000

    課題2提出

---
+++ kadai-02/index.js
@@ -0,0 +1,17 @@
+console.log('hello')
+
+const firstName = 'Shimomura'
+const familyName = '99'
+
+console.log(firstName + familyName) // Shimomura99
+
+const sum = 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10
+
+console.log(sum) // 55
+
+const dog = false
+const cat = true
+
+console.log(dog || cat) // true
+console.log(dog && cat) // false
+console.log(!dog && cat) // true
