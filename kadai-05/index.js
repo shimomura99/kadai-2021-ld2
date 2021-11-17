0ab5fe468a198fd61221639b6c5108d181859321
shimomura99 <shimomura99@users.noreply.github.com>
2021-11-17 04:47:52 +0000

    課題5提出

---
+++ kadai-05/index.js
@@ -0,0 +1,19 @@
+const profile = {
+    name: "田中",
+    age: 21,
+    hobby: "サッカー",
+    major: "社会学"
+}
+const dictionary = {
+    name: "名前",
+    age: "年齢",
+    hobby: "趣味",
+    major: "専攻"
+}
+
+const keys = Object.keys(profile)
+for (const key of keys) {
+    const value = profile[key]
+    const key_in_japanese = dictionary[key]
+    console.log(`私の${key_in_japanese}は${value}です。`)
+}
