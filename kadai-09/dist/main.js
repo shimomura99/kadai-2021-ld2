ea238ed9cd9be18fe800803d312a2bf46996b3eb
shimomura99 <nain9350m@gmail.com>
2022-01-14 06:33:39 +0000

    課題9提出

---
+++ kadai-09/dist/main.js
@@ -0,0 +1,1 @@
+(()=>{var o={902:o=>{o.exports=()=>{console.log("This is moduleA.")}},78:o=>{o.exports=()=>{console.log("This is moduleB.")}}},r={};function s(e){var t=r[e];if(void 0!==t)return t.exports;var i=r[e]={exports:{}};return o[e](i,i.exports,s),i.exports}(()=>{const o=s(902),r=s(78);o(),r()})()})();
